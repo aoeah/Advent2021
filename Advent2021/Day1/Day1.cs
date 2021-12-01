93ba4d89b8cf51c1c40f2957b7ea461540dd1259
Chris Eckhout <ceckhout@gmail.com>
2021-12-01 08:33:14 -0800

    Add project files.

---
+++ Advent2021/Day1/Day1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Advent2021.Day1NS
+{
+    public class Day1
+    {
+        public string Part1() 
+        {
+            bool first = true;
+            var lastValue = 0;
+            var result = 0;
+
+            foreach (var line in GetData())
+            {
+                var currentValue = int.Parse(line);
+
+                if (first)
+                {
+                    first = false;
+                    lastValue = currentValue;
+                    continue;
+                }
+
+                if (currentValue - lastValue > 0) result++;
+                lastValue = currentValue;
+            }
+
+            return result.ToString();
+        }
+
+        public string Part2() 
+        {
+            var result = 0;
+
+            var counters = new List<int>();
+            var runningNumber = 0;
+
+            foreach (var line in GetData())
+            {
+                var currentValue = int.Parse(line);
+                counters.Add(0);
+
+                for (var i = runningNumber; i >= 0 && i > runningNumber - 3; i--)
+                {
+                    counters[i] += currentValue;
+                }
+
+                if (runningNumber >= 3)
+                {
+                    if (counters[runningNumber - 3] < counters[runningNumber - 2]) result++;
+                }
+
+                runningNumber++;
+            }
+
+            return result.ToString();
+        }
+
+        private static IEnumerable<string> GetData()
+        {
+            var lines = File.ReadAllLines("Day1\\day1.txt");
+
+            return lines;
+        }
+    }
+}
---
+++ Advent2021/Program.cs
@@ -0,0 +1,18 @@
+﻿using Advent2021.Day1NS;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Advent2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var day = new Day1();
+
+            Console.WriteLine(day.Part2());
+        }
+    }
+}
+
